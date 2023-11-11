b3f15dd78ba850f7ae885501e5bf3499365301bf
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-11 14:47:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,15 @@ import { Filter } from './Filter/Filter';
 import s from '../App.module.css';
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    };
-  }
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
   componentDidMount() {
     try {
